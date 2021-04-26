c7600bd5edc1e085432f0cb8ce7ccb744758028d
Danny <dannyfarrington5@gmail.com>
2021-04-25 22:18:25 -0500

    - making some typo corrections

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -179,22 +179,22 @@ def plot_single_note_graphs(plot_data_dict):
 
 title_container = st.beta_container()
 title_container.title('Classifying the Genre of a Drum Beat With Machine Learning')
-intro_blurb = """Drumming, like all musical instruments, has common "stereo types" that change for each genre of music.
+intro_blurb = """Drumming, like all musical instruments, has common "stereotypes" that change for each genre of music.
 Using singing as an example, if you were to turn on a country song, you would expect to hear a southern twang in the singer's voice and some long held-out notes. But
-if you were to turn on a rap song, you would expect to hear fast and rhythmic singing. These common "stereo types" exist for drum beats as well. Rock beats are loud with a lot of space between notes,
+if you were to turn on a rap song, you would expect to hear fast and rhythmic singing. These common "stereotypes" exist for drum beats as well. Rock beats are loud with a lot of space between notes,
  while Latin grooves are fast and full of finesse.
 
- My goal with this personal project was to create a Machine Learning model that could intake an audio recording from my Electronic Drumset and classify the genre of the drum beat I played.
- I recorded 120 grooves on my Electronic Drumset, and did my best to capture the common stereo types of 4 different genres of music:
- Rock, Shuffle/Funk (SixEight groove), Latin, and Jazz. For each recording, I played 4 measures of a drum beat at varying speeds which resulted in about 9-15 seconds of drums per audio file.
-  My Electronic Drumset records at 44.1 khz which translates into 44.1k rows of data per second of audio (396.9k-661.5k rows per audio file). After transforming each audio file into meta data,
- an XGBoosted Classification model was trained to be able to correctly identify the genre of each Drum Beat at a 90% accuracy rating."""
+ My goal with this personal project was to create a machine learning model that could intake an audio recording from my electronic drumset and classify the genre of the drum beat I played.
+ I recorded 120 grooves on my electronic drumset, and did my best to capture the common stereo types of 4 different genres of music:
+ Rock, Shuffle/Funk (SixEight groove), Latin, and Jazz. For each recording, I played 4 measures of a drum beat at varying speeds which resulted in about 9-15 seconds of sound per audio file.
+  My electronic drumset records at 44.1 khz which translates into 44.1k rows of data per second of audio (396.9k-661.5k rows per audio file). After transforming each audio file into meta data,
+ an XGBoosted classification model was trained to be able to correctly identify the genre of each drum beat at a 90% accuracy rating."""
 
 genre_stereotypes = """The general stereotypes/motifs you can expect in each genre are as follows:
-- *Rock:* Big strong hits on the snare a bass drum, with consistent and generally large amounts of space between each note. The Hi-Hat fills in the space between the drum hits.
-- *Shuffle/Funk:* The snare drum contains loud hits with a notable amount of "soft" hits inbetween. More bass drum hits compared to rock. Lots of Hi-Hat notes that vary from soft to loud.
-- *Latin:* The bass drum follows an ever-constant "heart beat" pattern. A "cha-cha" pattern is played around the snare drum and toms. Fast and consistent notes are played on the cymbals. The Hi-Hat sports a constant "chick" (think boom-chick-boom-chick) pattern.
-- *Jazz:* There are little to no bass drum hits. Snare drum hits seem to be played at random and are meant as a form of embellishment, varying from soft to loud. A constant shuffle-like pattern is played on the cymbal with the Hi-Hat being played on the backbeat
+- *Rock:* Big strong hits on the snare a bass drum, with consistent and generally large amounts of space between each note. The hi-hat fills in the space between the drum hits.
+- *Shuffle/Funk:* The snare drum contains loud hits with a notable amount of "soft" hits inbetween. More bass drum hits compared to rock. Lots of hi-hat notes that vary from soft to loud.
+- *Latin:* The bass drum follows an ever-constant "heart beat" pattern. A "cha-cha" pattern is played around the snare drum and toms. Fast and consistent notes are played on the cymbals. The hi-hat sports a constant "chick" (think boom-chick-boom-chick) pattern.
+- *Jazz:* There are little to no bass drum hits. Snare drum hits seem to be played at random and are meant as a form of embellishment, varying from soft to loud. A constant shuffle-like pattern is played on the cymbal with the hi-hat being played on the backbeat
  (where you would normally expect to hear a snare drum in any rock beat)."""
 
 high_level_data_transformation_explanation = """The main goal of transforming the audio file data is to get it in a state to be able to capture the following information:
