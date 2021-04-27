a0fe54f25c2e3d5375dbef03675607934f92d4a3
Danny <dannyfarrington5@gmail.com>
2021-04-27 13:53:08 -0500

    - removing a space

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -264,7 +264,6 @@ with st.beta_expander('Data Transformation: Overview'):
 	st.write(high_level_data_transformation_explanation)
 st.write('')
 st.write('')
-st.write('')
 with st.beta_expander('Data Transformation: Step-By-Step'):
 	st.write(step_by_step_data_transformation_explanation)
 st.write('')
